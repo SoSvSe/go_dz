d5011939ed4cc9180765cfeab1254e74d188d742
Slavik <tronimeni@gmail.com>
2025-06-14 17:13:21 +0300

    коммит 2 дз

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,8 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	userInput := getUserInput()
+	conversion(userInput, "eur", "rub")
+	fmt.Print(userInput)
 	const usdEur float64 = 1.1
 	const usdRub float64 = 96.4
 	eurRub := usdRub / usdEur
 	fmt.Print(eurRub)
+}
+
+func getUserInput() float64 {
+	var userInput float64
+	fmt.Print("Ввод пользователя")
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func conversion(cash float64, original string, target string) {
+
 }
